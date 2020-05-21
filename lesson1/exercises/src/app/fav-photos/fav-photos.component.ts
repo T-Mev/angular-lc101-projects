100c13a2969a298145847e0dd8536dfed1671f71
Ted M <practice@example.com>
2020-05-21 15:27:31 -0400

    Completed Lesson 1, Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Look at these cool images:';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://blogthebloggers.files.wordpress.com/2010/06/rick-astley-2.jpg?w=584';
+  image3 = 'https://i.redd.it/bekphnqftcb41.jpg';
 
   constructor() { }
 
